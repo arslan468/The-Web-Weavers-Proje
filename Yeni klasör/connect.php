22fb48106dd9186bfbe92fdda5da4458315bdbde
Mehmet Alp ARSLAN <77723113+arslan468@users.noreply.github.com>
2023-06-07 14:42:47 +0300

    yeni klasör temizlendi

--- Yeni klasör/connect.php
+++
@@ -1,7 +0,0 @@
-<?php
-    try {
-        $connection = new PDO("mysql:host=localhost;dbname=insider","root", "");
-    } catch (\Throwable $th) {
-        echo "<h1>Bağlanamadı hata: ".$th."</h1>";
-    }
-?>
