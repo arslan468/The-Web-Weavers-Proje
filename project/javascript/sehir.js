de6c2c6b6801f99927ab9d0d9d211e08abb67cf3
Mehmet Alp ARSLAN <77723113+arslan468@users.noreply.github.com>
2023-06-08 18:23:19 +0300

    şehir sayfası bar yüzde back-end i bitti

--- project/javascript/sehir.js
+++ project/javascript/sehir.js
@@ -3,4 +3,34 @@ window.addEventListener('load', function() {   //page loaded event
         var inputValue =  $("#olive").val();
         console.log(inputValue);
     });
+document.querySelector("#muz-btn").addEventListener("click", function(){
+    let muzdeger = Number.parseInt(document.querySelector("#muz-deger").innerHTML);
+    let kdeger = Number.parseInt(document.querySelector("#txtmuz").value);
+    let muzbar = document.querySelector("#muzbar");
+
+
+    let sonuc = (muzdeger+kdeger);
+
+    if (sonuc > 95) {
+        document.querySelector("#muz-destek").style.color = "red";
+        document.querySelector("#muz-destek-d").style.color = "red";
+    }
+
+    let styles = window.getComputedStyle(muzbar);
+    let eskibardeger = Number.parseInt((parseFloat(styles.width) / muzbar.parentElement.offsetWidth) * 100);
+    if (muzdeger < 100) {
+        let yenibardeger = (eskibardeger+kdeger+2).toString();
+        muzbar.style = "width:"+yenibardeger+"%";
+        document.querySelector("#muz-deger").innerHTML = sonuc;
+    }
+    else
+    {
+        alert("İhtiyaç olan miktardan fazla ürün üretiyorsunuz!!!");
+    }
+
+   
+    
 });
+
+});
+
