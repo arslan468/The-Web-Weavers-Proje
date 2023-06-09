ff01da91fa0898d82902ba1546ba6dc6c3ce0f70
Mehmet Alp ARSLAN <77723113+arslan468@users.noreply.github.com>
2023-06-09 15:28:22 +0300

    şehir sayfası alert eklendi

--- project/javascript/sehir.js
+++ project/javascript/sehir.js
@@ -1,4 +1,6 @@
 window.addEventListener('load', function() {   //page loaded event
+
+    
     $("#olive").on("keyup", function(){
         var inputValue =  $("#olive").val();
         console.log(inputValue);
